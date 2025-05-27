5d4a2e9577dcaf8749cc40f28a9f71ed0674bf52
mruganka <mruganka.263@gmail.com>
2025-05-27 18:24:12 +0100

    Add beforeAll hook

--- src/test/hooks/hooks.js
+++ src/test/hooks/hooks.js
@@ -1,17 +1,50 @@
-const { Before, After, AfterStep } = require('@cucumber/cucumber');
+const { BeforeAll, Before, After, AfterStep, AfterAll } = require('@cucumber/cucumber');
 const { chromium } = require('playwright');
 const { POManager } = require('../pages/POManager');
 const { setDefaultTimeout } = require('@cucumber/cucumber');
+const shareState = require('./shareState');
 
 setDefaultTimeout(60 * 1000);
 
-Before(async function () {
-        const browser = await chromium.launch({ headless: false });
-        const context = await browser.newContext({ viewport: { width: 1440, height: 900 } });
-        this.page = await context.newPage();
+BeforeAll(async function () {
+        shareState.browser = await chromium.launch({ headless: false });
+        shareState.webContext = await shareState.browser.newContext();
+        let page = await shareState.webContext.newPage();
 
+        let poManager = new POManager(page);
+        let loginPage = poManager.getLoginPageObj(page);
+        await loginPage.launchURL()
+
+        /**
+         * Capturing response token after first login  
+         */
+        const [response] = await Promise.all([
+                page.waitForResponse(resp =>
+                        resp.url().includes('/realms/secure-test/protocol/openid-connect/token') && resp.status() === 200),
+                loginPage.loginToTheApplication("temp_user2@company2.com", "eqZ19#2!$%Irw%Gn")
+        ]);
+
+        /**
+         * Storing access token
+         */
+        const json = await response.json();
+        shareState.bearerToken = json.access_token;
+
+        /*
+        Storing the session and cookie details 
+        */
+        await shareState.webContext.storageState({ path: 'state.json' });
+
+})
+
+Before(async function () {
+        /*
+        * Opening browser with using same session id , instead of login 
+        */
+        shareState.browser = await chromium.launch({ headless: false });
+        shareState.webContext = await shareState.browser.newContext({ storageState: 'state.json', viewport: { width: 1440, height: 900 } })
+        this.page = await shareState.webContext.newPage();
         this.poManager = new POManager(this.page);
-        this.loginPage = this.poManager.getLoginPageObj(this.page);
 })
 
 After(async function () {
@@ -20,10 +53,12 @@ After(async function () {
 
 AfterStep(async function ({ pickle, result }) {
         // Only capture screenshots if a page is available and step did something
-        if (this.page.waitForLoadState() && result?.status !== 'skipped') {
+        if (this.page.waitForLoadState('domcontentloaded') && result?.status !== 'skipped') {
                 const buffer = await this.page.screenshot({ fullPage: true });
-
-                // Attach screenshot to report
-                await this.attach(buffer, 'image/png');
+                this.attach(buffer, 'image/png');
         }
 });
+AfterAll(async function () {
+        await shareState.webContext.close();
+        await shareState.browser.close();
+})
